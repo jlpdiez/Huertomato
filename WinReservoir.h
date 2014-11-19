4bc942155a4e0f39782b8c09bf960f2ee61571df
Juan Luis Pérez Díez <ender.vs.melkor@gmail.com>
2014-11-19 05:19:50 +0100

    Reuploaded WinReservoir

---
+++ WinReservoir.h
@@ -0,0 +1,38 @@
+// #############################################################################
+//
+// # Name       : WinReservoir
+// # Version    : 1.0
+//
+// # Author     : Juan L. Perez Diez <ender.vs.melkor at gmail>
+// # Date       : 19.11.2014
+//
+// # Description: Reservoir module submenu. On/off toggle, alarms, calibration and pump protection
+//
+// #  This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// #############################################################################
+
+#ifndef WINRESERVOIR_H_
+#define WINRESERVOIR_H_
+
+//TODO: Should have: ON/OFF toggle ¿first? (then  grey out or not remaining options)
+//Should be cool to autodetect if sensors are unresponsive and change setting.reservoirModule
+//And read hgere and grey out automatically
+//Alarms menu
+//Sensor Calibration
+//Pump protection
+
+
+
+#endif 
